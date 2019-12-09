8323aef35cb0b974a5526232850958ae4a74eaab
Tom Mendez-Porcel <mendezpo1u@etu.univ-lorraine.fr>
2019-12-09 13:50:39 +0100

    ajout des liens et du css

--- mywishlist/src/controleur/ControleurAffichage.php
+++ mywishlist/src/controleur/ControleurAffichage.php
@@ -4,16 +4,6 @@ namespace mywishlist\controleur;
 use \mywishlist\vue\VueParticipant;
 
 class ControleurAffichage{
-  public function racine(){
-    //A MODFIER
-    echo "<h2>Racine</h2>" ;
-    $app = \Slim\Slim::getInstance();
-    echo 'URL de la racine : '.$app->urlFor('racine').'<br>';
-    echo 'URL de getListes : '.$app->urlFor('getListes').'<br>';
-    echo 'URL de getListe : '.$app->urlFor('getListe', ['id'=>0]).'<br>';
-    echo 'URL de getItem : '.$app->urlFor('getItem', ['id'=>0]).'<br>';
-  }
-
   public function afficherLesListes(){
     $listl = \mywishlist\models\Liste::all();
     $vue= new VueParticipant($listl->toArray());
@@ -30,5 +20,10 @@ class ControleurAffichage{
     $item = \mywishlist\models\Item::find($id);
     $vue = new VueParticipant([$item]);
     $vue->render(3);
+  }
+
+  public function racine(){
+    $vue= new VueParticipant(null);
+    $vue->render(4);
   }
 }
--- mywishlist/src/vue/VueParticipant.php
+++ mywishlist/src/vue/VueParticipant.php
@@ -9,9 +9,10 @@ class VueParticipant {
   }
 
   private function afficherLesListes(){
-    $content="";
+    $app=\Slim\Slim::getInstance();
+    $content="<h2>Listes</h2>";
     foreach($this->arr as $l){
-      $content.="<li>".$l["no"].' '.$l["titre"]."</li>";
+      $content.="<li>".$l["no"].' <a href="'.$app->urlFor('getListe', ['id'=>$l["no"]]).'">'.$l["titre"]."</a></li>";
     }
 
     $html = <<<END
@@ -21,22 +22,22 @@ END;
   }
 
   private function afficherListe($liste){
-    $html = "";
+    $app = \Slim\Slim::getInstance();
+    $html = "<h2>Liste</h2>";
     $l = \mywishlist\models\Liste::find($liste["no"]);
     $items=$l->items()->get();
     foreach ($items as $item) {
       $html .= '<li>';
       $URI = \Slim\Slim::getInstance()->request->getRootURI();
       $html .= "<img src=\"$URI/web/img/{$item->img}\" width=\"60\" height=\"60\">";
-      $html .= $item->nom.' - ';
-      $html .= $item->descr;
+      $html .= '<a href="'.$app->urlFor('getItem', ['id'=>$item["id"]]).'">'.$item->nom.'</a>';
       $html .= '</li>';
     }
     return $html;
   }
 
   private function afficherItem($item){
-    $html="";
+    $html="<h2>Item</h2>";
     $nomitem=$item["img"];
     $URI = \Slim\Slim::getInstance()->request->getRootURI();
     $html .= "<img src=\"$URI/web/img/$nomitem\" width=\"60\" height=\"60\">";
@@ -46,7 +47,16 @@ END;
     return $html;
   }
 
+  private function racine(){
+    $app = \Slim\Slim::getInstance();
+    $html = "<h2>Racine</h2>" ;
+    $html .= 'Accès aux listes : <a href="'.$app->urlFor('getListes').'">listes</a><br>';
+
+    return $html;
+  }
+
   public function render($selecteur){
+    $app = \Slim\Slim::getInstance();
     switch ($selecteur) {
       case 1: {
         $content = $this->afficherLesListes();
@@ -60,16 +70,29 @@ END;
         $content = $this->afficherItem($this->arr[0]);
         break;
       }
+
+      case 4 : {
+        $content = $this->racine();
+        break;
+      }
     }
+    $urlRacine=$app->urlFor('racine');
+    $urlCSS=$app->request->getRootURI().'/web/style.css';
     $html = <<<END
 <!DOCTYPE html>
 <html>
-<head><title>AfficherLesListes</title></head>
+<head>
+  <title>MyWishList</title>
+  <link rel="stylesheet" type="text/css" href="$urlCSS">
+</head>
 <body>
-<div class="content">
- $content
-</div>
-</body><html>
+  <h1>MyWishList</h1>
+  <div class="content">
+   $content
+  </div>
+  <footer><p>URL de la racine : <a href="$urlRacine">racine</a><p></footer>
+</body>
+<html>
 END;
   echo $html;
   }
