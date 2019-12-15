af9b2e1f7f16d28daaf8df7bf01419af71c4983a
Théo <theo.pennerat@gmail.com>
2019-12-15 12:39:35 +0100

    ajout compte dans mywishlist.sql

--- mywishlist/mywishlist.sql
+++ mywishlist/mywishlist.sql
@@ -51,3 +51,9 @@ INSERT INTO `liste` (`no`, `user_id`, `titre`, `description`, `expiration`, `tok
 (1,	1,	'Pour fêter le bac !',	'Pour un week-end à Nancy qui nous fera oublier les épreuves. ',	'2018-06-27',	'nosecure1'),
 (2,	2,	'Liste de mariage d\'Alice et Bob',	'Nous souhaitons passer un week-end royal à Nancy pour notre lune de miel :)',	'2018-06-30',	'nosecure2'),
 (3,	3,	'C\'est l\'anniversaire de Charlie',	'Pour lui préparer une fête dont il se souviendra :)',	'2017-12-12',	'nosecure3');
+
+create table Compte(
+    login varchar(30) not null,
+    password varchar(60) not null,
+    PRIMARY KEY(login)
+);
